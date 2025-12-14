a1b5569a2fdf277c5b341ec3021f6ec737b2ea7e
siah <i@siah.sg>
2025-12-14 13:06:53 +0800

    feat: basic frosty glass

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -5,12 +5,20 @@ import {
 	useGLTF,
 	useTexture,
 } from "@react-three/drei";
+import { useEffect } from "react";
 import * as THREE from "three";
 
 const bgColo2 = new THREE.Color("#234a99");
 
 export function Globe() {
 	const scene = useControls((a) => a.SCENE);
+	const normalTex = useTexture("/frost_normal.jpg");
+	const frostTex = useTexture("/globe-frost.jpg");
+
+	useEffect(() => {
+		frostTex.flipY = false;
+		frostTex.needsUpdate = true;
+	}, [frostTex]);
 
 	const [props] = useSpring(() => {
 		return {
@@ -31,12 +39,27 @@ export function Globe() {
 			>
 				<MeshTransmissionMaterial
 					color="#fff"
+					normalMap={normalTex}
+					normalScale={0.15}
 					transmission={0.98}
 					thickness={0.2}
 					background={bgColo2}
+				/>
+			</mesh>
+			<mesh
+				// @ts-expect-error -- geometry ts type
+				geometry={nodes.sphere.geometry}
+				scale={10.5}
+			>
+				<meshBasicMaterial
+					color="#fff"
+					alphaMap={frostTex}
+					transparent={true}
 				/>
 			</mesh>
 		</animated.group>
 	);
 }
 useGLTF.preload("/jar.glb");
+useTexture.preload("/frost_normal.jpg");
+useTexture.preload("/globe-frost.jpg");
